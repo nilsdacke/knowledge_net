74b423b2b9c67e1546d010ffbb1de27f3c5fc399
nilsdacke <j.s.andersson@gmail.com>
2023-12-01 10:38:21 +0100

    Change build_database.py parameters

--- build_database.py
+++ build_database.py
@@ -5,14 +5,12 @@ from knowledge_net.experimental.database.database import Database
 from knowledge_net.experimental.database.document_processor import TextDocumentProcessor
 from credentials import openai_api_key
 
-if len(sys.argv) != 2:
-    print("Usage: python build_database.py <knowledgebase name>")
+if len(sys.argv) != 3:
+    print("Usage: python build_database.py <document directory> <db directory>")
     sys.exit()
 
-knowledgebase_name = sys.argv[1]
+document_path = Path(sys.argv[1])
+database_path = Path(sys.argv[2])
 
 processor = TextDocumentProcessor()
-document_path = Path(f"examples/documents/{knowledgebase_name}")
-database_path = Path(f"db/{knowledgebase_name}")
-
 Database(database_path, openai_api_key=openai_api_key).build_from_folder(document_path, processor)
